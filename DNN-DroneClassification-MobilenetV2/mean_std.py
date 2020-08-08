c87683d4be75b4bb65a381e39dc6a5c46a0f07f6
gbrao018 <gbrao017@gmail.com>
2020-08-08 15:39:22 +0530

    mean_std, test_train_utils, colab training file

--- DNN-DroneClassification-MobilenetV2/mean_std.py
+++ DNN-DroneClassification-MobilenetV2/mean_std.py
@@ -5,9 +5,31 @@ from torch.utils.data import DataLoader, Dataset
 #std= tensor([0.1707, 0.1650, 0.1804])
 #(tensor([0.5271, 0.5788, 0.6095]), tensor([0.1707, 0.1650, 0.1804]))
 
+import torch
+from torchvision import transforms
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+#Assuming test data represent the full set of train data. So we use normalized values of traion data in test data also.
+
+train_root_dir = "/content/drone-dataset/train"
+processed_train_root_dir = "/content/processed-drone-dataset/train"
+
+tr = transforms.Compose([
+    #transforms.RandomHorizontalFlip(),
+    #transforms.RandomCrop(),
+    transforms.ToTensor()
+    #normalize,
+    #transforms.RandomErasing()
+])
+
+
+train_data_set = preprocess_images(train_root_dir, processed_train_root_dir, transform = tr)
+
 def mean_std(dataset):
   dataloader = torch.utils.data.DataLoader(dataset, batch_size=1,
-                                          shuffle=True, num_workers=1)
+                                          shuffle=False, num_workers=1)
+
   mean = torch.zeros(3)
   std = torch.zeros(3)
   print('Computing mean and std from dataloader')
@@ -20,3 +42,14 @@ def mean_std(dataset):
   print('mean=',mean)
   print('std=',std)
   return mean, std
+
+print('calculating mean,std of train dataset')
+mean_std(train_data_set)
+
+test_root_dir = "/content/drone-dataset/test"
+processed_test_root_dir = "/content/processed-drone-dataset/test"
+
+test_data_set = PreprocessImages(test_root_dir, processed_test_root_dir, transform = tr)
+
+print('calculating mean,std of test dataset')
+mean_std(test_data_set)
